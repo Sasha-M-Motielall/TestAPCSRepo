e89d25d4a52596b08fb84a0510451534182a800a
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-11-22 15:15:06 -0500

    array deep to string

--- Demo.java
+++ Demo.java
@@ -1,4 +1,5 @@
 public class Demo{
+
   public static void main(String args[]){
     for(int i=1;i<=5;i++){
       for(int j=1;j<=6-i;j++){
@@ -24,5 +25,17 @@ public class Demo{
       convert = "{" + convert + "}";
       return (convert);
     }
+
+    public static String arrayDeepToString (int[][] arr) {
+     String conv = "";
+     if (arr.length == 0) {
+       return "{}";
+     }
+     for (int i=0; i < arr.length-1; i++) {
+       conv += arrToString(arr[i]) + ", ";
+     }
+     conv = "{" + conv + arrToString(arr[arr.length-1]) + "}";
+     return conv;
+   }
 
 }
